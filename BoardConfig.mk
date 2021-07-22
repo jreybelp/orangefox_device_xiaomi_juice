bf2c968cb64aca40b188dfe2eece7b6a53d23009
Darth9 <reyprimerjefe@gmail.com>
2021-07-22 04:36:45 +0000

    juice: Fix MIUI 12.x Android 11 decryption when the build system is not appropriately patched

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,3 +139,7 @@ TARGET_USES_MKE2FS := true
 TW_EXCLUDE_TWRPAPP := true
 TW_NO_SCREEN_BLANK := true
 PLATFORM_VERSION := 20.1.0
+
+BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += \
+    --prop com.android.build.boot.security_patch:$(PLATFORM_SECURITY_PATCH) \
+    --prop com.android.build.boot.os_version:$(PLATFORM_VERSION)
